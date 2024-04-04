46c364b3b6f58d3438b3c057f6c42ceff905b222
RiluoW <1959016551@qq.com>
2024-04-04 21:20:48 +0800

    ddd

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+   <input type="text" v-model="text">
+   <button @click="test">add</button>
+   <ul>
+    <li v-for="(data,index) in list" :key="index">{{ data }}</li>
+   </ul>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      text:'',
+      list:[]
+    }
+  },
+  methods:{
+    test(){
+      this.list.push(this.text)
+    }
+  }
+}
+</script>
