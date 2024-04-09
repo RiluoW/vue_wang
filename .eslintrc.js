bfb1053d6b9d14cfd00d72726ab35458a4a783b9
RiluoW <1959016551@qq.com>
2024-04-09 23:34:16 +0800

    0409

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
-module.exports = {
-  requireConfigFile:"false",
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+// module.exports = {
+//   requireConfigFile:"false",
+//   root: true,
+//   env: {
+//     node: true
+//   },
+//   extends: [
+//     'plugin:vue/essential',
+//     '@vue/standard'
+//   ],
+//   parserOptions: {
+//     parser: '@babel/eslint-parser'
+//   },
+//   rules: {
+//     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+//     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+//   }
+// }
